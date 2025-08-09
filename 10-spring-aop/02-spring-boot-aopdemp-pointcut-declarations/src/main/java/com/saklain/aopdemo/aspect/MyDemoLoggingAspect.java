1b92cf1e3849134dd47077d58cea3e235ebf7ce0
saklain-s <123095572+saklain-s@users.noreply.github.com>
2025-08-10 00:37:44 +0530

    Pointcut Declarations with multiple uses

--- 10-spring-aop/02-spring-boot-aopdemp-pointcut-declarations/src/main/java/com/saklain/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-aop/02-spring-boot-aopdemp-pointcut-declarations/src/main/java/com/saklain/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,26 +2,35 @@ package com.saklain.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
-    // this is where we add all of our related advices for logging
+    @Pointcut("execution(* com.saklain.aopdemo.dao.*.*(..))")
+    private void forDaoPackage(){}
 
-    // let's start with a @Before advice
-    // @Before("execution(public void updateAccount())")
-    // @Before("execution(public void com.saklain.aopdemo.dao.AccountDAO.addAccount())") this adds to a particular method only
-    // @Before("execution(public void add*())") add to every method that starts with the name add
-    // @Before("execution( void add*())") // applied to only void modifier classes
-    //@Before("execution(* add*())") // applied to only void modifier classes
-    // @Before("execution(* add*(com.saklain.aopdemo.Account))") // applied to only void modifier classes
-    // @Before("execution(* add*(com.saklain.aopdemo.Account, ..))")
-    // @Before("execution(* add*(..))")
-     @Before("execution(* com.saklain.aopdemo.dao.*.*(..))")
+    // create a pointcut for getter methods
+    @Pointcut("execution(* com.saklain.aopdemo.dao.*.get*(..))")
+    private void getter(){}
+
+    // create a pointcut for setter methods
+    @Pointcut("execution(* com.saklain.aopdemo.dao.*.set*(..))")
+    private void setter(){}
+
+    // create a pointcut: include package ... exclude getter/setter
+    @Pointcut("forDaoPackage() && !(getter()) || setter()")
+    private void forDaoPackageNoGetterSetter(){}
+
+     @Before("forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=====>>> Execution @Before advice on method");
+    }
 
+    @Before("forDaoPackageNoGetterSetter()")
+    public void performApiAnalytics(){
+        System.out.println("\n=====>>> Performing API analytics");
     }
 }
